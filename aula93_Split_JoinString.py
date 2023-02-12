3b1059e8f81fa391d8a187dc78cd898793a66751
Dario Calçada <dariobcalcada@gmail.com>
2023-02-12 16:48:23 -0300

    commit split 2

--- aula93_Split_JoinString.py
+++ aula93_Split_JoinString.py
@@ -3,8 +3,8 @@ split e join com list e str
 split - divide uma string (list)
 join - une uma string
 """
-frase = '   Olha só que   , coisa interessante          '
-lista_frases_cruas = frase.split(',')
+frase = 'Olha só que, coisa interessante'
+lista_frases_cruas = frase.split()
 
 lista_frases = []
 for i, frase in enumerate(lista_frases_cruas):
