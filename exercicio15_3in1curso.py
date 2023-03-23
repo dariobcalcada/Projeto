b89a9dd4ffa54fe7ddf5710b766f9ed65af1fbb4
Dario Calçada <dariobcalcada@gmail.com>
2023-03-23 18:15:44 -0300

    commit update adiar funcao

--- exercicio15_3in1curso.py
+++ exercicio15_3in1curso.py
@@ -1,6 +1,6 @@
 import copy
 
-from dados import produtos
+# from dados import produtos
 
 # copy, sorted, produtos.sort
 # Exercícios
@@ -12,6 +12,8 @@ produtos = [
     {'nome': 'Produto 3', 'preco': 10.11},
     {'nome': 'Produto 2', 'preco': 105.87},
     {'nome': 'Produto 4', 'preco': 69.90},
+]
+
 novos_produtos = [
     {**p, 'preco': round(p['preco'] * 1.1, 2)}
     for p in copy.deepcopy(produtos)
