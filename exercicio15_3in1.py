054758779bcce6882b769a0c62630cc1275feb28
dariobcalcada <dariobcalcada@gmail.com>
2023-03-23 17:00:08 -0300

    commit exercise 15 start

--- exercicio15_3in1.py
+++ exercicio15_3in1.py
@@ -15,3 +15,30 @@ produtos = [
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+produtos_mais_dez = produtos
+
+
+
+produtos_preco_cresc = produtos
+
+for produto in produtos_mais_dez:
+    preco = produto.get('preco')
+    preco *= 1.1
+    produto['preco'] = preco
+    
+print('*** Essa é a lista de produtos com aumento de 10% no preço ***')
+print(produtos_mais_dez)
+print()
+print()
+
+produtos_nome_decrescente = produtos
+produtos_nome_decrescente = sorted(produtos)
+
+print(produtos_nome_decrescente)
+
+
+
+
+
+
