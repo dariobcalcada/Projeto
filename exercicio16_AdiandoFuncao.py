82c34c83518c127a012f41a166990ec27715fed3
Dario Calçada <dariobcalcada@gmail.com>
2023-03-28 15:01:01 -0300

    commit intern funtion

--- exercicio16_AdiandoFuncao.py
+++ exercicio16_AdiandoFuncao.py
@@ -6,6 +6,9 @@ def soma(x, y):
 def multiplica(x, y):
     return x * y
 
+def potencia(x,y):
+    return x**y
+
 
 def criar_funcao(funcao, x):
     def interna(y):
@@ -15,6 +18,10 @@ def criar_funcao(funcao, x):
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
+potencia_de_dois = criar_funcao(potencia,2)
+potencia_de_tres = criar_funcao(potencia,3)
 
 print(soma_com_cinco(10))
 print(multiplica_por_dez(10))
+print(potencia_de_dois(3))
+print(potencia_de_tres(2))
